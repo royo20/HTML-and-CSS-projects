eaa0d84f580b80af52669b069f0a51235f62627c
Rojina Yazdani <rojina.yazdani@gmail.com>
2020-12-20 13:56:14 -0800

    Basic javaScript commands

---
+++ JavaScript-projects/Project1_expressions_alert/JavaScript_File.js
@@ -0,0 +1,14 @@
+window.alert("alert!"); //windows alert pop-up
+
+document.write("Hello World!"); //display text
+
+var Name = "Kathy" + "Smith"; //assign variable and concatnate
+
+var x = "hello" //assign a variable 
+var x = x.fontcolor("red") //assign a font color
+document.write(x); //print x
+
+document.write(2+2); //addition expression
+
+
+
